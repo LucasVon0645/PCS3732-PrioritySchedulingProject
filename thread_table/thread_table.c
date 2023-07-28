0d91754a0453038d3e1b5aebb47281f3e45d0c6e
franos-pj <francisco.mariani@polijunior.com.br>
2023-07-28 11:33:40 -0300

    UPDATE: thread_table

--- thread_table/thread_table.c
+++ thread_table/thread_table.c
@@ -1,26 +1,57 @@
 #include <stdint.h>
 #include "thread_table.h"
 
-int delete_from_table(MultilevelQueue* multi_queue) {
-    for(int i = 0; i < NUM_OF_QUEUES; i++) {
-        if (!isEmpty(multi_queue->queues[i])) return dequeue(multi_queue->queues[i]);
+void add_to_table(thread_table* table, tcb* new_tcb) {
+    node *new_node = {new_tcb, NULL, NULL};
+
+    if (table->list_head == NULL) {
+        table->list_head = new_node;
+        table->list_end = new_node;
+        new_node->next_node = new_node;
+        new_node->previous_node = new_node;
+        return;
     }
-    return 0;
+
+    new_node->next_node = table->list_head;
+    new_node->previous_node = table->list_end;
+    table->list_end->next_node = new_node;
+    table->list_head->previous_node = new_node;
+    table->list_end = new_node;
 }
 
-int enqueue_by_priority(int tid, int priority, MultilevelQueue* multi_queue) {
-    Queue* queue_to_use = multi_queue->queues[priority];
-    if (isFull(queue_to_use)) {
-        return 0;
-    }
-    enqueue(multi_queue->queues[priority], tid);
-    return 1;
-}
+int delete_from_table_by_id(thread_table* table, int tid) {
+    node* current_node = table->list_head;
+    int current_tid = current_node->tcb_t->tid;
 
-int dequeue_by_priority(int priority, MultilevelQueue* multi_queue) {
-    Queue* queue_to_use = multi_queue->queues[priority];
-    if (isEmpty(queue_to_use)) {
-        return 0;
+    while (current_tid != tid) {
+        current_node = current_node->next_node;
+        if (current_node == table->list_end) return 0;
+        current_tid = current_node->tcb_t->tid;
     }
-    return dequeue(multi_queue->queues[priority]);
+
+    node* previous_node = current_node->previous_node;
+    node* next_node = current_node->next_node;
+
+    // Single element in the list
+    if (next_node == current_node) {
+        table->list_head = NULL;
+        table->list_end = NULL;
+
+    }
+    // Only two elements in the list
+    else if (next_node == previous_node) {
+        next_node->next_node = NULL;
+        next_node->previous_node = NULL;
+        table->list_head = next_node;
+        table->list_end = next_node;
+    }
+    // Arbitrary number of elements in the list
+    else {
+        previous_node->next_node = next_node;
+        next_node->previous_node = previous_node;
+        if (table->list_head == current_node) table->list_head = next_node;
+        if (table->list_end == current_node) table->list_end = previous_node;
+    }
+
+    return current_node->tcb_t->tid;
 }
--- thread_table/thread_table.h
+++ thread_table/thread_table.h
@@ -2,16 +2,26 @@
 
 #define MAX_THREAD_NUM 10
 
-// Estrutura do Thread Control Block (TCB).
 typedef struct {
     uint32_t regs[17];   // contexto (17 registradores)
-    uint32_t tid;        // identificador do processo
-    uint32_t priority;   // prioridade atual do processo
-    uint32_t exc_slots;  // numero de execucoes
+    uint32_t tid;        // identificador da thread
+    uint32_t priority;   // prioridade atual da thread
+    uint32_t exc_slots;  // numero de execucoes restantes
 } tcb;
+
+typedef struct {
+    tcb* tcb_t;
+    node* next_node;
+    node* previous_node;
+} node;
 
  
 // Estrutura da tabela de TCBs
 typedef struct {
-    tcb* threads[MAX_THREAD_NUM];
+    node* list_head;
+    node* list_end;
 } thread_table;
+
+
+void add_to_table(thread_table* table, tcb* new_tcb);
+int delete_from_table_by_id(thread_table* table, int tid);
